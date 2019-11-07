b45884192649d58bc1ad28c04fddb2f40280d4bf
MathijsKeunen <mathijs.keunen@student.kuleuven.be>
2019-11-07 21:24:28 +0100

    created level template

---
+++ levels/template/FlagController.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+var reached_flags = {}
+
+signal all_flags_reached
+
+func _ready():
+	for child in get_children():
+		reached_flags[child.color] = false
+		child.connect("entered",self,"_on_flag_entered")
+		child.connect("exited",self,"_on_flag_exited")
+
+func _on_flag_entered(color):
+	reached_flags[color] = true
+	if reached_flags.values().count(true) == reached_flags.values().size():
+		emit_signal("all_flags_reached")
+
+func _on_flag_exited(color):
+	reached_flags[color] = false
---
+++ levels/template/laser.gd
@@ -0,0 +1,24 @@
+extends TileMap
+
+export var color = "blue"
+export var active = true
+
+var collision_layer_when_active = 0
+
+func _ready():
+	set_tileset(load("res://environment items/lasers/tilesets/"+color+".tres"))
+# warning-ignore:return_value_discarded
+	owner.connect("collision_initialized",self,"_set_collision")
+
+# warning-ignore:unused_argument
+func _set_collision(player_collision_bits_map, wall_collision_bits_map):
+	
+	for bit in wall_collision_bits_map.values():
+		collision_layer_when_active += pow(2,bit)
+	
+	collision_layer_when_active -= pow(2,wall_collision_bits_map[color])
+	_set_active(active)
+
+func _set_active(active):
+	set_collision_layer(collision_layer_when_active if active else 0)
+	set_visible(active)
